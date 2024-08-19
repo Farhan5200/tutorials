9569eeff9b379f844373a87030d64321d40d25d4
mohammed farhan <mohammedfarhan221farhan@gmail.com>
2024-08-19 08:44:17 +0530

    2 push

--- school_management/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    state = fields.Selection(selection_add=[('admitted', 'Admitted')])
