433a5a23f54a01626faefd2c9145acfa15d3ca54
Farhan5200 <mohammedfarhan221farhan@gmail.com>
2024-09-05 07:16:41 +0530

    task10 room mrng

--- school_management/wizard/school_exam_report_wizard.py
+++ school_management/wizard/school_exam_report_wizard.py
@@ -8,6 +8,22 @@ class SchoolExamReportWizard(models.TransientModel):
 
     exam_id = fields.Many2one('school.exam', string="Exam")
     class_id = fields.Many2one('school.class', string="Class")
+    exam_domain_ids = fields.Many2many('school.exam', compute="_compute_exam_domain_ids")
+
+
+    @api.onchange('class_id')
+    def _onchange_class_id(self):
+        self.exam_id = False
+
+    @api.depends('class_id')
+    def _compute_exam_domain_ids(self):
+        for rec in self:
+            if not rec.class_id:
+                rec.exam_domain_ids = rec.env['school.exam'].search([])
+            else:
+                rec.exam_domain_ids = rec.env['school.exam'].search([('class_id', '=', rec.class_id.id)])
+
+
 
 
     def action_print_report(self):
