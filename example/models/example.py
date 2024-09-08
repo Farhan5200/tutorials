dc5e034a48a2b7ba66be9032ac068ee71f5dc921
Farhan5200 <mohammedfarhan221farhan@gmail.com>
2024-09-08 22:15:06 +0530

    task 11 room

--- example/models/example.py
+++ example/models/example.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from audioop import reverse
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Example(models.Model):
@@ -13,109 +13,122 @@ class Example(models.Model):
     line_ids = fields.One2many('example.line', 'example_id')
     tag_ids = fields.Many2many('example.tag')
 
-    def action_button(self):
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     print(vals_list,'vv')
+    #     print(self,'ss')
+    #     # super(vals_list).create(vals_list)
+
+    #     super.create()
+    def update(self):
+        print('hi')
+
+    def action_button(self,vals_list):
         """To perform x2many value filling concept"""
-        print("helloo")
-        limit= [0,0]
-        # sum = 0
-        quantity = 0
-        # for rec in self.line_ids.filtered(lambda x:x.quantity >= 10):
-        #     # if rec.quantity >= 10:
-        #        limit[0] += rec.price
-        #        limit[1] += rec.quantity
-        # 
-        # # limit.append(sum)
-        # # limit.append(quantity)
-        # 
-        # hii = self.line_ids.filtered(lambda x:x.quantity >= 10).sorted('quantity').mapped('quantity')
+        print(vals_list)
+        print('jiiiiiiii')
+        # return x
+        # # print("helloo")
+        # # limit= [0,0]
+        # # sum = 0
+        # quantity = 0
+        # # for rec in self.line_ids.filtered(lambda x:x.quantity >= 10):
+        # #     # if rec.quantity >= 10:
+        # #        limit[0] += rec.price
+        # #        limit[1] += rec.quantity
+        # #
+        # # # limit.append(sum)
+        # # # limit.append(quantity)
+        # #
+        # hii = self.line_ids.filtered(lambda x:x.quantity >= 10).sorted('quantity')
         # print(hii)
-        # print(sum(hii))
-        print(self.line_ids.search([('quantity', '>=', 10)]))
-        print(self.line_ids.filtered(lambda x:x.quantity >= 10))
-
-
-        # product_id = self.env['product.product'].search([], order='id desc', limit=1)
-        # print(product_id.name)
+        # # print(sum(hii))
+        # # print(self.line_ids.search([('quantity', '>=', 10)]))
+        # # print(self.line_ids.filtered(lambda x:x.quantity >= 10))
+        #
+        #
+        # # product_id = self.env['product.product'].search([], order='id desc', limit=1)
+        # # print(product_id.name)
+        # # # fields.Command.create(values)
+        # # first Method
+        # # self.line_ids = [fields.Command.create({
+        # #     'product_id': product_id.id,
+        # #     'price': product_id.lst_price,
+        # #     'quantity': 5
+        # # })]
+        #
+        # # self.write({
+        # #     'line_ids': [fields.Command.create({
+        # #         'product_id': product_id.id,
+        # #         'price': product_id.lst_price,
+        # #         'quantity': 5
+        # #     })]
+        # # })
+        #
+        # # self.line_ids.create({
+        # #     'product_id': product_id.id,
+        # #     'price': product_id.lst_price,
+        # #     'quantity': 5,
+        # #     'example_id' : self.id
+        # # })
+        #
+        # # fields.Command.update(id,values)
+        # line_id = self.line_ids[-1]
+        # # self.line_ids = [fields.Command.update(line_id.id,{
+        # #     # 'product_id': product_id.id,
+        # #     # 'price': product_id.lst_price,
+        # #     'quantity': 10
+        # # })]
+        # #
+        # # line_id.write({
+        # #     'quantity': 10,
+        # # })
+        # # fields.Command.delete(id)
+        # # self.line_ids = [fields.Command.delete(line_id.id)]
+        # #
+        # # line_id.unlink()
         # # fields.Command.create(values)
-        # first Method
-        # self.line_ids = [fields.Command.create({
-        #     'product_id': product_id.id,
-        #     'price': product_id.lst_price,
-        #     'quantity': 5
-        # })]
-
-        # self.write({
-        #     'line_ids': [fields.Command.create({
-        #         'product_id': product_id.id,
-        #         'price': product_id.lst_price,
-        #         'quantity': 5
-        #     })]
-        # })
-
-        # self.line_ids.create({
-        #     'product_id': product_id.id,
-        #     'price': product_id.lst_price,
-        #     'quantity': 5,
-        #     'example_id' : self.id
-        # })
-
-        # fields.Command.update(id,values)
-        line_id = self.line_ids[-1]
-        # self.line_ids = [fields.Command.update(line_id.id,{
-        #     # 'product_id': product_id.id,
-        #     # 'price': product_id.lst_price,
-        #     'quantity': 10
-        # })]
+        # # first Method
+        # # self.line_ids = [fields.Command.create({
+        # #     'product_id': product_id.id,
+        # #     'price': product_id.lst_price,
+        # #     'quantity': 5
+        # # })]
         #
-        # line_id.write({
-        #     'quantity': 10,
-        # })
-        # fields.Command.delete(id)
-        # self.line_ids = [fields.Command.delete(line_id.id)]
+        # # self.write({
+        # #     'line_ids': [fields.Command.create({
+        # #         'product_id': product_id.id,
+        # #         'price': product_id.lst_price,
+        # #         'quantity': 5
+        # #     })]
+        # # })
         #
-        # line_id.unlink()
-        # fields.Command.create(values)
-        # first Method
-        # self.line_ids = [fields.Command.create({
-        #     'product_id': product_id.id,
-        #     'price': product_id.lst_price,
-        #     'quantity': 5
-        # })]
-
-        # self.write({
-        #     'line_ids': [fields.Command.create({
-        #         'product_id': product_id.id,
-        #         'price': product_id.lst_price,
-        #         'quantity': 5
-        #     })]
-        # })
-
-        # self.line_ids.create({
-        #     'product_id': product_id.id,
-        #     'price': product_id.lst_price,
-        #     'quantity': 5,
-        #     'example_id' : self.id
-        # })
-
-        # # fields.Command.link(id)
-        # tag_id = self.tag_ids.search([('name','=','new')],limit=1)
-        # self.tag_ids = [fields.Command.link(tag_id.id)]
-
-        empty_line_id = self.line_ids.search([('example_id','=',False),('example_example_id','=',False)])
-        # print("aaaa",empty_line_id.product_id.name)
-        # self.line_ids =[fields.Command.unlink(self.line_ids[-1].id)]
-
-        # fields.Command.clear()
-        # self.line_ids =[fields.Command.clear()]
-
-        # fields.Command.set(ids)
-        # self.line_ids = [fields.Command.set(empty_line_id.ids)]
-
-
+        # # self.line_ids.create({
+        # #     'product_id': product_id.id,
+        # #     'price': product_id.lst_price,
+        # #     'quantity': 5,
+        # #     'example_id' : self.id
+        # # })
         #
-        # fields.Command.unlink(id)
-        # tag_id = self.tag_ids.search([('name', '=', 'new')], limit=1)
-        # self.tag_ids = [fields.Command.unlink(tag_id.id)]
+        # # # fields.Command.link(id)
+        # # tag_id = self.tag_ids.search([('name','=','new')],limit=1)
+        # # self.tag_ids = [fields.Command.link(tag_id.id)]
+        #
+        # empty_line_id = self.line_ids.search([('example_id','=',False),('example_example_id','=',False)])
+        # # print("aaaa",empty_line_id.product_id.name)
+        # # self.line_ids =[fields.Command.unlink(self.line_ids[-1].id)]
+        #
+        # # fields.Command.clear()
+        # # self.line_ids =[fields.Command.clear()]
+        #
+        # # fields.Command.set(ids)
+        # # self.line_ids = [fields.Command.set(empty_line_id.ids)]
+        #
+        #
+        # #
+        # # fields.Command.unlink(id)
+        # # tag_id = self.tag_ids.search([('name', '=', 'new')], limit=1)
+        # # self.tag_ids = [fields.Command.unlink(tag_id.id)]
 
 class ExampleLine(models.Model):
     """ To learn one2many"""
--- school_management/wizard/school_student_report_wizard.py
+++ school_management/wizard/school_student_report_wizard.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import html2text
 from odoo import api, fields,models
+import io
+import json
+
+from odoo.exceptions import ValidationError
+from odoo.tools import date_utils
+
+try:
+   from odoo.tools.misc import xlsxwriter
+except ImportError:
+   import xlsxwriter
 
 
 class SchoolStudentReportWizard(models.TransientModel):
@@ -14,8 +25,6 @@ class SchoolStudentReportWizard(models.TransientModel):
     class_domain_ids = fields.Many2many('school.class', compute="_compute_department_id_class_id")
     student_id = fields.Many2one('student.registration', string="Student")
     student_domain_ids = fields.Many2many('student.registration', compute='_compute_department_id_class_id')
-
-
 
     @api.depends('department_id', 'class_id')
     def _compute_department_id_class_id(self):
@@ -40,7 +49,7 @@ class SchoolStudentReportWizard(models.TransientModel):
         self.student_id = False
 
 
-    def action_print_report(self):
+    def action_print_report_pdf(self):
         """For printing student report"""
 
         data={
@@ -51,3 +60,182 @@ class SchoolStudentReportWizard(models.TransientModel):
 
         return self.env.ref('school_management.action_report_school_student').report_action(None, data=data)
 
+    def action_print_report_xlsx(self):
+        """to print xlsx report"""
+        data = {
+            'class_name': self.class_id.name,
+            'department_name': self.department_id.name,
+            'student_name': self.student_id.first_name
+        }
+        return {
+            'type': 'ir.actions.report',
+            'data': {'model': 'school.student.report.wizard',
+                     'options': json.dumps(data,
+                                           default=date_utils.json_default),
+                     'output_format': 'xlsx',
+                     'report_name': 'Excel Report',
+                     },
+            'report_type': 'xlsx',
+        }
+
+    def get_xlsx_report(self, data, response):
+        """to create xlsx report"""
+        class_name = data['class_name']
+        department_name = data['department_name']
+        student_name = data['student_name']
+        selected_values = {
+            'selected_class': class_name,
+            'selected_department': department_name,
+            'selected_student': student_name
+        }
+        company_details = html2text.html2text(self.env.company.company_details)
+
+        query = """select student_registration.name as admission_no, student_registration.first_name as name,school_department.id as department_id, 
+                school_class.name as class_name, school_department.name as department_name, student_registration.gender as 
+                gender from((student_registration inner join school_class on school_class.id = 
+                student_registration.current_class_id)inner join school_department on school_class.department_id = 
+                school_department.id)"""
+
+        department_query = "select id as dep_id, name as dept_name from school_department"
+
+        class_query = "select name as class_name, id as class_id, department_id from school_class"
+
+        if class_name and department_name:
+            if student_name:
+                query += " where student_registration.first_name = '%s'" % student_name
+            else:
+                query += " where school_class.name = '%s' and school_department.name = '%s'" % (
+                class_name, department_name)
+        if class_name and not department_name:
+            if student_name:
+                query += " where student_registration.first_name = '%s'" % student_name
+            else:
+                query += " where school_class.name = '%s'" % class_name
+        if not class_name and department_name:
+            if student_name:
+                query += " where student_registration.first_name = '%s'" % student_name
+            else:
+                query += " where school_department.name = '%s'" % department_name
+        if not class_name and not department_name and student_name:
+            query += " where student_registration.first_name = '%s'" % student_name
+
+        self.env.cr.execute(query)
+        report = self.env.cr.dictfetchall()
+        self.env.cr.execute(department_query)
+        department_details = self.env.cr.dictfetchall()
+        self.env.cr.execute(class_query)
+        class_details = self.env.cr.dictfetchall()
+
+        if report:
+            output = io.BytesIO()
+            workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+            sheet = workbook.add_worksheet()
+            cell_format = workbook.add_format(
+                {'font_size': '12px', 'align': 'center'})
+            head = workbook.add_format(
+                {'align': 'center', 'bold': True, 'font_size': '20px'})
+            txt = workbook.add_format({'font_size': '10px', 'align': 'center'})
+
+            sheet.merge_range('A1:F6', company_details, cell_format)
+            sheet.merge_range('B7:I8', 'STUDENT REPORT', head)
+
+            if student_name:
+                # Heading
+                sheet.merge_range('A10:B10', 'Admission Number', cell_format)
+                sheet.merge_range('C10:D10', 'Name', cell_format)
+                sheet.merge_range('E10:F10', 'Gender', cell_format)
+                sheet.write('G10', 'Class', cell_format)
+                sheet.merge_range('H10:I10', 'Department', cell_format)
+
+                # Body
+                row = 10
+                for rec in report:
+                    row += 1
+                    sheet.merge_range(f'A{row}:B{row}', rec['admission_no'], txt)
+                    sheet.merge_range(f'C{row}:D{row}', rec['name'], txt)
+                    sheet.merge_range(f'E{row}:F{row}', rec['gender'], txt)
+                    sheet.write(f'G{row}', rec['class_name'], txt)
+                    sheet.merge_range(f'H{row}:I{row}', rec['department_name'], txt)
+
+            elif department_name and class_name:
+                # Head
+                sheet.merge_range('A10:C10', f'Department Name : {department_name}', cell_format)
+                sheet.merge_range('A11:C11', f'Class Name : {class_name}', cell_format)
+                sheet.merge_range('A13:B13', 'Admission Number', cell_format)
+                sheet.merge_range('C13:D13', 'Name', cell_format)
+                sheet.merge_range('E13:F13', 'Gender', cell_format)
+
+                # Body
+                row = 13
+                for rec in report:
+                    row += 1
+                    sheet.merge_range(f'A{row}:B{row}', rec['admission_no'], txt)
+                    sheet.merge_range(f'C{row}:D{row}', rec['name'], txt)
+                    sheet.merge_range(f'E{row}:F{row}', rec['gender'], txt)
+
+            elif not department_name and not class_name:
+                row = 10
+                for dep in department_details:
+                    for cls in class_details:
+                        if dep['dep_id'] == cls['department_id']:
+                            sheet.merge_range(f'A{row}:C{row}', f'Department Name : {dep["dept_name"]}', cell_format)
+                            row += 1
+                            sheet.merge_range(f'A{row}:C{row}', f'Class Name : {cls["class_name"]}', cell_format)
+                            row += 2
+                            sheet.merge_range(f'A{row}:B{row}', 'Admission Number', cell_format)
+                            sheet.merge_range(f'C{row}:D{row}', 'Name', cell_format)
+                            sheet.merge_range(f'E{row}:F{row}', 'Gender', cell_format)
+                            for rec in report:
+                                if cls['class_name'] == rec['class_name']:
+                                    row += 1
+                                    sheet.merge_range(f'A{row}:B{row}', rec['admission_no'], txt)
+                                    sheet.merge_range(f'C{row}:D{row}', rec['name'], txt)
+                                    sheet.merge_range(f'E{row}:F{row}', rec['gender'], txt)
+                            row += 3
+
+            elif not department_name and class_name:
+                row = 10
+                for dep in department_details:
+                    for cls in class_details:
+                        if dep['dep_id'] == cls['department_id'] and cls['class_name'] == class_name:
+                            sheet.merge_range(f'A{row}:C{row}', f'Department Name : {dep["dept_name"]}', cell_format)
+                            row += 1
+                            sheet.merge_range(f'A{row}:C{row}', f'Class Name : {cls["class_name"]}', cell_format)
+                            row += 2
+                            sheet.merge_range(f'A{row}:B{row}', 'Admission Number', cell_format)
+                            sheet.merge_range(f'C{row}:D{row}', 'Name', cell_format)
+                            sheet.merge_range(f'E{row}:F{row}', 'Gender', cell_format)
+
+                            for rec in report:
+                                if dep['dep_id'] == rec['department_id'] and cls['class_name'] == class_name:
+                                    row += 1
+                                    sheet.merge_range(f'A{row}:B{row}', rec['admission_no'], txt)
+                                    sheet.merge_range(f'C{row}:D{row}', rec['name'], txt)
+                                    sheet.merge_range(f'E{row}:F{row}', rec['gender'], txt)
+                            row += 3
+
+            elif department_name and not class_name:
+                row = 10
+                for dep in department_details:
+                    for cls in class_details:
+                        if dep['dep_id'] == cls['department_id'] and dep['dept_name'] == department_name:
+                            sheet.merge_range(f'A{row}:C{row}', f'Department Name : {dep["dept_name"]}', cell_format)
+                            row += 1
+                            sheet.merge_range(f'A{row}:C{row}', f'Class Name : {cls["class_name"]}', cell_format)
+                            row += 2
+                            sheet.merge_range(f'A{row}:B{row}', 'Admission Number', cell_format)
+                            sheet.merge_range(f'C{row}:D{row}', 'Name', cell_format)
+                            sheet.merge_range(f'E{row}:F{row}', 'Gender', cell_format)
+                            for rec in report:
+                                if cls['class_name'] == rec['class_name'] and dep['dept_name'] == department_name:
+                                    row += 1
+                                    sheet.merge_range(f'A{row}:B{row}', rec['admission_no'], txt)
+                                    sheet.merge_range(f'C{row}:D{row}', rec['name'], txt)
+                                    sheet.merge_range(f'E{row}:F{row}', rec['gender'], txt)
+                            row += 3
+            workbook.close()
+            output.seek(0)
+            response.stream.write(output.read())
+            output.close()
+        else:
+            raise ValidationError('There are no records...!')
